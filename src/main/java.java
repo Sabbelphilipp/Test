c0ff3d35c32d853bb391b2a37847f29efafab0ac
Reto Giger <reto.giger@students.fhnw.ch>
2015-10-05 21:41:28 +0200

    first commit

---
+++ src/main/java.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by Reto Giger on 05.10.2015.
+ */
+public class java {
+    public static void main(String[] args) {
+        System.out.println("JAVA!");
+    }
+}
